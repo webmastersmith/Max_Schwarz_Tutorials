00fc40c35e5fbae44705dc83d14a8522ef4ccd0c
Bryon Smith <bryon@smithauto.us>
2022-01-17 13:00:35 -0600

    minor change

--- cra/src/pages/meetups/meetupUtils.tsx
+++ cra/src/pages/meetups/meetupUtils.tsx
@@ -19,9 +19,9 @@ export const MeetupItem = ({
 }): JSX.Element => {
   return (
     <li>
-      {/* <div>
-    <img src={image} alt={title} />
-  </div> */}
+      <div>
+        <img src={image} alt={title} />
+      </div>
       <div>
         <h3>{title}</h3>
         <address>{address}</address>
