c211c4c97341e23208bba334b3f951f47a33438e
Bryon Smith <bryon@smithauto.us>
2022-01-17 16:31:49 -0600

    minor change

--- cra/src/pages/meetups/newMeetups/NewMeetUps.tsx
+++ cra/src/pages/meetups/newMeetups/NewMeetUps.tsx
@@ -1,11 +1,14 @@
 import { FC } from 'react'
 import { NewMeetupForm } from './NewMeetupForm'
+import styles from './NewMeetupForm.module.scss'
 
 export const NewMeetUpsPage: FC = (): JSX.Element => {
   return (
-    <section>
-      <h1>Add New Meetup</h1>
-      <NewMeetupForm />
+    <section className={styles.formWrapper}>
+      <div>
+        <h1>Add New Meetup</h1>
+        <NewMeetupForm />
+      </div>
     </section>
   )
 }
--- cra/src/pages/meetups/newMeetups/NewMeetupForm.tsx
+++ cra/src/pages/meetups/newMeetups/NewMeetupForm.tsx
@@ -1,4 +1,4 @@
-import { FC } from 'react'
+import { FC, FormEvent } from 'react'
 import { MeetupCard } from 'pages'
 import styles from './NewMeetupForm.module.scss'
 
@@ -16,9 +16,28 @@ export const NewMeetupForm: FC<{}> = (): JSX.Element => {
     { id: 'address', msg: 'Address', type: 'text', required: true },
     { id: 'description', msg: 'Description', type: 'text', required: true },
   ]
+
+  interface SubmitData {
+    title: HTMLInputElement
+    image: HTMLInputElement
+    address: HTMLInputElement
+    description: HTMLInputElement
+  }
+  function handleSubmit(e: FormEvent<HTMLFormElement>): void {
+    e.preventDefault()
+    // each item will be html node.
+    const { title, image, address, description } = e.target as typeof e.target &
+      SubmitData
+    console.log({
+      title: title.value,
+      image: image.value,
+      address: address.value,
+      description: description.value,
+    })
+  }
   return (
     <MeetupCard>
-      <form className={styles.form}>
+      <form className={styles.form} onSubmit={handleSubmit}>
         {formData.map(({ id, msg, type, required }: FormData) => {
           return (
             <div>
