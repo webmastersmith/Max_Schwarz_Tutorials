f0bc0d2d1f6487fad3d4066b1dcdfba8f194160b
Bryon Smith <bryon@smithauto.us>
2022-01-27 18:52:19 -0600

    minor change

--- event-planner/pages/_app.tsx
+++ event-planner/pages/_app.tsx
@@ -7,6 +7,7 @@ function MyApp({ Component, pageProps }: AppProps) {
   return (
     <Layout>
       <Head>
+        <title>Events of Greatness!</title>
         <meta name="viewport" content="initial-scale=1.0, width=device-width" />
       </Head>
       <Component {...pageProps} />
