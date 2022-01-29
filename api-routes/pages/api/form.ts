419037c9fa9f7f1e88a25ca603ab24c1eed4a3da
Bryon Smith <bryon@smithauto.us>
2022-01-29 14:08:26 -0600

    setup form

---
+++ api-routes/pages/api/form.ts
@@ -0,0 +1,26 @@
+// Next.js API route support: https://nextjs.org/docs/api-routes/introduction
+import type { NextApiRequest, NextApiResponse } from 'next'
+import fs from 'fs'
+import path from 'path'
+
+type Data = {
+  [key: string]: string
+}
+
+export default function handler(
+  req: NextApiRequest,
+  res: NextApiResponse<Data>
+) {
+  if (req.method === 'POST') {
+    // const { email, feedback } = JSON.parse(req.body)
+    const filePath = path.join(process.cwd(), 'data', 'db.json')
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    data.push(JSON.parse(req.body))
+    fs.writeFileSync(filePath, JSON.stringify(data))
+    res.status(201).json({ name: 'Great! Thanks' })
+  } else {
+    const filePath = path.join(process.cwd(), 'data', 'db.json')
+    const data = JSON.parse(fs.readFileSync(filePath, 'utf-8'))
+    res.status(200).json(data)
+  }
+}
--- api-routes/pages/index.tsx
+++ api-routes/pages/index.tsx
@@ -1,7 +1,61 @@
 import type { NextPage } from 'next'
+import Link from 'next/link'
+import { useState } from 'react'
 
 const Home: NextPage = () => {
-  return <div>The Home Page</div>
+  const [data, setData] = useState([])
+
+  const handleSubmit = async (event: any) => {
+    event.preventDefault()
+    const formData = new FormData(event.target)
+    const dataObject = Object.fromEntries(formData)
+    console.log('dataObject', dataObject) //{email: "bob@gmail.com", feedback: "hghg!!"}
+    await fetch('http://localhost:3000/api/form', {
+      method: 'POST',
+      body: JSON.stringify(dataObject),
+    })
+      .then((res) => res.json())
+      .then((data) => console.log(data))
+
+    // event.target.reset() //reset form.
+  }
+
+  const handleClick = () => {
+    fetch('http://localhost:3000/api/form')
+      .then((res) => res.json())
+      .then((data) => setData(data))
+  }
+
+  return (
+    <div>
+      <h1>Home Page</h1>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <label htmlFor="email">Your Email Address</label>
+          <input type="email" name="email" id="email" required />
+        </div>
+
+        <div>
+          <label htmlFor="feedback">Your Feedback</label>
+          <input type="text" name="feedback" id="feedback" />
+        </div>
+        <button type="submit">Submit</button>
+      </form>
+      <hr />
+      <button type="button" onClick={handleClick}>
+        View All
+      </button>
+      {!!data.length &&
+        data.map((d: any, i: number) => {
+          return (
+            <div key={i}>
+              <p>{d.email}</p>
+              <p>{d.feedback}</p>
+            </div>
+          )
+        })}
+    </div>
+  )
 }
 
 export default Home
