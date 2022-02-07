f91c94da78afe68777ffb83e3035f1b83d58568b
Bryon Smith <bryon@smithauto.us>
2022-02-07 11:12:45 -0600

    blog tutorial initial setup

---
+++ blog/pages/index.tsx
@@ -0,0 +1,10 @@
+import type { NextPage } from 'next'
+import Head from 'next/head'
+import Image from 'next/image'
+import styles from '../styles/HomePage.module.css'
+
+const HomePage: NextPage = () => {
+  return <div className="container"></div>
+}
+
+export default HomePage
