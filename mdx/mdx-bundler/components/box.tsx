d70d660ce4a1aa3d18d05d0542f5f2d5bba49f97
Bryon Smith <bryon@smithauto.us>
2022-02-14 11:04:46 -0600

    mdx-bundler test

--- mdx/mdx-bundler/components/box.tsx
+++ mdx/mdx-bundler/components/box.tsx
@@ -1,5 +1,5 @@
 import * as React from 'react'
 //import styles from './Box.module.scss'
 
-const Box = () => <div>Box</div>
+const Box = ({ title }: any) => <div>Box {title}</div>
 export default Box
--- mdx/mdx-bundler/pages/index.tsx
+++ mdx/mdx-bundler/pages/index.tsx
@@ -1,15 +1,14 @@
 import type { NextPage } from 'next'
-import Head from 'next/head'
 import Image from 'next/image'
 import styles from '../styles/Home.module.css'
-import fs from 'fs'
-import path from 'path'
 import { getMDXComponent } from 'mdx-bundler/client'
 import * as React from 'react'
 import { getCompiledMDX, getFileContent } from 'utils'
+import { getPlaiceholder } from 'plaiceholder'
 
-const Paragraph: React.FC<any> = (node) => {
+const Paragraph = (node: any) => {
   //props: children: { type: 'img', props: { src: 'woods.jpg', alt: "woods.jpg" }}
+  console.log('node', node)
 
   if (typeof node.children !== 'string' && node.children.type === 'img') {
     const {
@@ -23,33 +22,37 @@ const Paragraph: React.FC<any> = (node) => {
     const isPriority = props.alt
       ? props.alt.toLowerCase().includes('(priority)')
       : false
-    const isFill = props.alt ? /fill|responsive/.test(props.alt) : false
     console.log('isPriority', isPriority)
-    console.log('isFill', isFill)
     const size = props.alt.match(/\((\d*)x(\d*)\)/)
     console.log('size', size)
     const width = size ? size[1] : '768'
     const height = size ? size[2] : '432'
     console.log('width, height', width, height)
 
-    if (isFill) {
-      const stretch = props.alt.match(/\([^x]*x([^x]*)\)/)
-      console.log('stretch', stretch)
+    // <> {} / must be escaped in mdx.
+    const blurDataURL = props.alt.match(/data:[\w/\\;,=]+/)
+    console.log('dataBlur', blurDataURL)
 
-      return (
-        // <div style={{ position: 'relative', height: '500px' }}>
-        <div>
-          <Image
-            src={`/images/${props.src}`}
-            alt={alt}
-            priority={isPriority}
-            layout={'responsive'}
-            width={width}
-            height={200}
-          />
-        </div>
-      )
-    }
+    //   const isFill = props.alt ? /fill|responsive/.test(props.alt) : false
+    //   console.log('isFill', isFill)
+    // if (isFill) {
+    //   const stretch = props.alt.match(/\([^x]*x([^x]*)\)/)
+    //   console.log('stretch', stretch)
+
+    //   return (
+    //     // <div style={{ position: 'relative', height: '500px' }}>
+    //     <div>
+    //       <Image
+    //         src={`/images/${props.src}`}
+    //         alt={alt}
+    //         priority={isPriority}
+    //         layout={'responsive'}
+    //         width={width}
+    //         height={200}
+    //       />
+    //     </div>
+    //   )
+    // }
     return (
       <div>
         <Image
@@ -58,6 +61,8 @@ const Paragraph: React.FC<any> = (node) => {
           width={width}
           height={height}
           priority={isPriority}
+          placeholder="blur"
+          blurDataURL={blurDataURL}
         />
       </div>
     )
@@ -87,10 +92,12 @@ const Paragraph: React.FC<any> = (node) => {
 
 // const Img = (props: any) => {
 //   const { src, alt } = props
-//   return <Image src={src} alt={alt} width={300} height={200} />
+//   console.log('props', props)
+
+//   return <Image src={`/images/${src}`} alt={alt} width={300} height={200} />
 // }
 
-export const components = {
+const components = {
   // img: Img,
   p: Paragraph,
   // a: Anchor,
@@ -105,9 +112,10 @@ export const components = {
   // }
 }
 
-const Home: NextPage = ({ code, frontmatter }: any) => {
+const Home = ({ code, frontmatter, blurDataURL }: any) => {
   // console.log(code)
   console.log('frontmatterrrrr', frontmatter)
+  console.log('blurDataURL', blurDataURL)
 
   const Component = React.useMemo(() => getMDXComponent(code), [code])
 
@@ -123,9 +131,12 @@ export default Home
 
 export const getStaticProps = async () => {
   const data = await getCompiledMDX(getFileContent('example.mdx'))
+  const { base64 } = await getPlaiceholder(`/images/woods.jpg`)
+  console.log('base64', base64)
+
   // console.log('data', data)
 
   return {
-    props: { ...data },
+    props: { ...data, blurDataURL: base64 },
   }
 }
